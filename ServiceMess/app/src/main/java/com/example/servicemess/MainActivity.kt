ff2bd1c304d855370c010490c9615dab5e897ed5
Denis Zhuchkov <denis.bugir@gmail.com>
2024-11-01 16:00:51 +0300

    with Service Messenger

---
+++ ServiceMess/app/src/main/java/com/example/servicemess/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.example.servicemess
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.os.RemoteException
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var myService: Messenger? = null
+    private lateinit var myMessenger: Messenger
+    private var isServiceBound = false
+    private var isCounterRunning = false
+    private lateinit var countdownTextView: TextView
+    private lateinit var startServiceButton: Button
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            myService = Messenger(service)
+            isServiceBound = true
+            sendMessageToService(MSG_IS_RUNNING)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            myService = null
+            isServiceBound = false
+        }
+    }
+
+    inner class ResponseHandler(
+    ) : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG_START -> {
+                    isCounterRunning = true;
+                    startPolling()
+                    startServiceButton.text = getString(R.string.restart_service)
+                    updateScreenCounter(msg.arg1)
+                }
+                MSG_RESTART ->
+                    updateScreenCounter(msg.arg1)
+                MSG_GET_COUNTER ->
+                    updateScreenCounter(msg.arg1)
+                MSG_IS_RUNNING ->
+                    if (msg.arg1 == 1) {
+                        isCounterRunning = true
+                        startPolling()
+                        startServiceButton.text = getString(R.string.restart_service)
+                    } else {
+                        isCounterRunning = false
+                        startServiceButton.text = getString(R.string.start_service)
+                    }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        countdownTextView = findViewById(R.id.countdownTextView)
+        startServiceButton = findViewById(R.id.startServiceButton)
+
+        myMessenger = Messenger(ResponseHandler())
+        startService()
+
+        startServiceButton.setOnClickListener {
+            if (isCounterRunning) {
+                sendMessageToService(MSG_RESTART)
+            } else {
+                sendMessageToService(MSG_START)
+            }
+        }
+
+    }
+
+    private fun startService() {
+        val serviceIntent = Intent(this, MyService::class.java)
+        this.startService(serviceIntent)
+        bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun startPolling() {
+        handler.post(object : Runnable {
+            override fun run() {
+                if (isCounterRunning) {
+                    sendMessageToService(MSG_GET_COUNTER)
+                    handler.postDelayed(this, 1000)
+                }
+            }
+        })
+    }
+
+    private fun updateScreenCounter(counter: Int) {
+        if (counter > 0) {
+            countdownTextView.text = counter.toString()
+        }
+        else {
+            //Counter run out. Stop polling
+            startServiceButton.text = getString(R.string.start_service)
+            countdownTextView.text = ""
+            isCounterRunning = false
+        }
+    }
+
+    private fun sendMessageToService(messageId: Int) {
+        if (!isServiceBound) return
+        val msg: Message = Message.obtain(null, messageId, 0, 0)
+        msg.replyTo = myMessenger
+        try {
+            myService?.send(msg)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService()
+    }
+
+    private fun unbindService() {
+        if (isServiceBound) {
+            unbindService(connection)
+            isServiceBound = false
+        }
+    }
+}
---
+++ ServiceMess/app/src/main/java/com/example/servicemess/MyService.kt
@@ -0,0 +1,131 @@
+package com.example.servicemess
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.os.RemoteException
+import androidx.core.app.NotificationCompat
+
+
+const val MSG_START = 1
+const val MSG_RESTART = 2
+const val MSG_GET_COUNTER = 3
+const val MSG_IS_RUNNING = 4
+
+class MyService : Service() {
+
+    private lateinit var mMessenger: Messenger
+    private val handler = Handler(Looper.getMainLooper())
+    private val channelId = "CHANNEL_ID"
+    private val notificationId = 1
+
+    private lateinit var notificationIntent : Intent
+    private lateinit var pendingIntent : PendingIntent
+    private lateinit var notificationManager: NotificationManager
+
+    var isRunning = false
+    private var counter = 21
+
+    private fun sendMessageToClient(what: Int, replyArg: Int, clientMessenger: Messenger?) {
+        val message = Message.obtain(null, what, replyArg, 0)
+        try {
+            clientMessenger?.send(message)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+    }
+
+    inner class IncomingHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            val clientMessenger = msg.replyTo
+            val replyArg = when (msg.what) {
+                MSG_START -> startCountdown()
+                MSG_RESTART -> restartCountdown()
+                MSG_GET_COUNTER -> getCurrentProgress()
+                MSG_IS_RUNNING -> if (isRunning) 1 else 0
+                else -> return
+            }
+            sendMessageToClient(msg.what, replyArg, clientMessenger)
+        }
+    }
+
+    override fun onCreate() {
+        val channel = NotificationChannel(
+            channelId,
+            "Foreground Service Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+        notificationIntent = Intent(this, MainActivity::class.java)
+        pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        mMessenger = Messenger(IncomingHandler())
+    }
+
+    override fun onDestroy() {
+        notificationManager.deleteNotificationChannel(channelId)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY
+    }
+    override fun onBind(intent: Intent): IBinder {
+        return mMessenger.binder
+    }
+
+    fun getCurrentProgress(): Int {
+        return counter
+    }
+
+    fun restartCountdown(): Int {
+        counter = 20
+        return counter
+    }
+
+    fun startCountdown(): Int {
+        counter = 20
+        isRunning = true
+        startForegroundService()
+        handler.post(object : Runnable {
+            override fun run() {
+                if (counter > 0) {
+                    handler.postDelayed(this, 1000)
+                    counter--
+                    updateForegroundServiceCounter(counter)
+                }
+                else {
+                    isRunning = false
+                    stopForeground(STOP_FOREGROUND_REMOVE)
+                }
+            }
+        })
+        return counter
+    }
+    private fun startForegroundService() {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.progress) + counter)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(notificationId, notification)
+    }
+    private fun updateForegroundServiceCounter(counter : Int) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.progress) + counter)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(notificationId, notification)
+    }
+}
