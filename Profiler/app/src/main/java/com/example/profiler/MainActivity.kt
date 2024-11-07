2313c95978f5357c71c865786f92ef9d437f382b
Denis Zhuchkov <denis.bugir@gmail.com>
2024-11-07 15:26:27 +0300

    profiler

---
+++ Profiler/app/src/main/java/com/example/profiler/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.profiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var startWorkButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startWorkButton = findViewById(R.id.startWorkButton)
+
+        startWorkButton.setOnClickListener {
+
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .build()
+
+            val myWorkRequest: OneTimeWorkRequest =
+                OneTimeWorkRequestBuilder<MyWorker>()
+                    .setConstraints(constraints)
+                    .build()
+
+            WorkManager.getInstance(this)
+                .enqueueUniqueWork("unique work",
+                    ExistingWorkPolicy.KEEP,
+                    myWorkRequest)
+        }
+    }
+}
---
+++ Profiler/app/src/main/java/com/example/profiler/MyWorker.kt
@@ -0,0 +1,32 @@
+package com.example.profiler
+
+import android.content.Context
+import android.widget.Toast
+import androidx.work.CoroutineWorker
+import androidx.work.Data
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class MyWorker(appContext: Context, workerParams: WorkerParameters):
+    CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result {
+        withContext(Dispatchers.Main) {
+            val toast = Toast.makeText(applicationContext, "Started Work", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        val veryBigData = ByteArray(10 * 1024 )
+        delay(10000)
+
+        val resultData = Data.Builder().putByte("workResult", veryBigData[10]).build()
+
+        withContext(Dispatchers.Main) {
+            val toast = Toast.makeText(applicationContext, "Done some Work", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        return Result.success(resultData)
+    }
+}
